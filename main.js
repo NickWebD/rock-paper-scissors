5cbde4c97eaf37be0df2de917794ed37a18a26f8
Nikolay <nickwebdev89@gmail.com>
2024-03-20 19:59:48 +0300

    Added a function to define a payer choice in main.js

--- main.js
+++ main.js
@@ -5,3 +5,28 @@ function getComputerChoice() {
   let computerChoice = choices[Math.floor(Math.random() * 3)];
   return computerChoice;
 }
+
+//function to define a payer choice
+function getPlayerChoice() {
+  const choices = ["rock", "paper", "scissors"];
+
+  while (true) {
+    let playerChoice = prompt("Chose: rock, paper, scissors");
+
+    if (playerChoice === null) {
+      alert("Bye!");
+      return;
+    }
+
+    playerChoice = playerChoice.toLowerCase();
+
+    if (choices.includes(playerChoice)) {
+      return playerChoice;
+    } else {
+      alert("You NEED to choose: rock, paper, scissors");
+    }
+  }
+}
+console.log(getPlayerChoice());
+//function to play a single round of RPS
+//function playRound(playerSelection, computerSelection) {}
