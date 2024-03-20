aedd6b5acbb85fd709ff57099db6290295d1c5ee
Nikolay <nickwebdev89@gmail.com>
2024-03-20 18:07:09 +0300

    Added function to define a computer choice

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
 "use strict";
+// function to define a computer choice
+function getComputerChoice() {
+  const choices = ["rock", "paper", "scissors"];
+  let computerChoice = choices[Math.floor(Math.random() * 3)];
+  return computerChoice;
+}
